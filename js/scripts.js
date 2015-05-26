6f6cd118d42a3bfbe49848cd0dd6f51426eaa306
Alec Arme & alex caste <student@epicodus.com>
2015-05-26 14:33:10 -0700

    add front end html for factorial code

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,18 @@ return sum;
   // }
   // return sum;
 };
+
+$(document).ready(function() {
+  $("form#factorial").submit(function(event) {
+    var number = parseInt($("input#number").val());
+    var result = factorial(number)
+
+      $(".result").text(result);
+
+      $("#result").show();
+
+
+    event.preventDefault();
+
+  });
+});
