8c21ddaef7e74dee513f98b987612af349c8558e
Alec Arme & alex caste <student@epicodus.com>
2015-05-26 14:21:46 -0700

    Refactored code to use recursion.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 var factorial = function(number) {
-  var sum = 1
 
-  for (var number;number > 0;number--) {
-    sum = sum * number
+//recursive solution
+var sum = 1
+  if (number > 0) {
+    sum = number * factorial(number-1);
   }
+return sum;
 
-  return sum;
-
+ // non recursive solution
+  // var sum = 1
+  // for (var number;number > 0;number--) {
+  //   sum = sum * number
+  // }
+  // return sum;
 };
